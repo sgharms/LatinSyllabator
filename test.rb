02face2c2f4e21493c4152f6745c43b10e1d5775
Steven G. Harms <stharms@cisco.com>
2009-09-03 16:18:12 -0500

    made smaller text bodies for tests

--- test.rb
+++ test.rb
@@ -10,12 +10,18 @@ class LineTest < Test::Unit::TestCase
     setup do
       @notated_line = "Arma virumque cano trojae qui primus ab oris"
       @nocomma_line = "Arma virumque cano, trojae qui primus ab oris"
+      @shorter      = "Arma vir"
       @syllab_line  = "Ar,,ma ,,vi,,rum,,que ,,ca,,no ,,tro,,jae ,,qui pri,,mu,,s a,,b o,,ris"
     end
 
-    should "Syllabation should be the same" do
-      assert_equal @syllab_line, syllabate(@nocomma_line).to_s
+    should "Simple test of word 'arma'" do
+      assert_equal "Ar,,ma", syllabate("Arma").to_s
     end
+    
+
+    # should "Syllabation should be the same" do
+    #   assert_equal @syllab_line, syllabate(@shorter).to_s
+    # end
   end
 end
 
